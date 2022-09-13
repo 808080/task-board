c027a8b3301f49cd0001a9016821d7c760dad3a3
szaderaka <sergey.zaderaka@quadcode.com>
2022-09-14 00:27:41 +0300

    init commit

---
+++ src/lib/Button.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let type: 'button' | 'submit' | 'reset';
+  export let text: string;
+  export let classList: string = '';
+</script>
+
+<button on:click {type} class={classList}>
+  {text}
+</button>
+
+<style>
+
+</style>
