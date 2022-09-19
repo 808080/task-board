b0de923d9c2d8cc394eb12ec377ec679e6f004fd
szaderaka <sergey.zaderaka@quadcode.com>
2022-09-19 14:12:20 +0300

    prevent dnd on image drag

--- src/utils/column.ts
+++
@@ -1,6 +0,0 @@
-import { writable } from "svelte/store";
-import type { Column } from "./types";
-
-const ColumnContext = writable<Column>(null);
-
-export default ColumnContext;
--- src/utils/modal.ts
+++
@@ -1,6 +0,0 @@
-import type { SvelteComponent } from 'svelte';
-import { writable } from 'svelte/store';
-
-const modal = writable<typeof SvelteComponent>(null);
-
-export default modal;
